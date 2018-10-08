8a9c3fc8c8107fd1f413c90f18a80393c1d3b047
grant smith <grantsmith1256@gmail.com>
2018-10-08 21:15:22 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -29,8 +29,8 @@ def until_iterator(number_of_times)
   counter+=1
 end
 
-def for_iterator(number_of_times)
-  for phrase in 1..7 do
-    puts "Welcome to Flatiron School's Web Development Course!"
-  end
-end
+ def for_iterator(number_of_times)
+ for phrase in 1..7 do
+    puts "Welcome to Flatiron School's Web Development Course!" 
+ end
+ end
