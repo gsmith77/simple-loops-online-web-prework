b2622e0cda567ae394c2ee3dcae60c155e905f32
grant smith <grantsmith1256@gmail.com>
2018-10-08 21:03:06 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,8 +11,7 @@ end
 
 def times_iterator(number_of_times)
   7.times do
-    puts "Welcome to Flatiron School's Web Development
-    Course!"
+    puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
 
